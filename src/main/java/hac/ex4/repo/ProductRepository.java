8ad19b28d004eb2e4b5a265b5938fca3de791fa9
HadasMu <hadas0125@gmail.com>
2022-06-07 15:10:22 +0300

    admin side

--- src/main/java/hac/ex4/repo/ProductRepository.java
+++ src/main/java/hac/ex4/repo/ProductRepository.java
@@ -7,6 +7,7 @@ import java.util.List;
 /* default scope of this Bean is "singleton" */
 public interface ProductRepository extends JpaRepository<Product, Long> {
 
+
     List<Product> findByName(String name);
     /* add here the queries you may need - in addition to CRUD operations
     List<User> findUserByUserName(String userName);
